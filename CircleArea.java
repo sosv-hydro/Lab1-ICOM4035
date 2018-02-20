3fea7ca89fdbcfa9b3c7f964d2d3442d73d8a016
sosv-hydro <sofia.saavedra1@upr.edu>
2018-02-19 20:40:51 -0400

    Modified class to also include calculation of half the area of circle

--- CircleArea.java
+++ CircleArea.java
@@ -7,12 +7,18 @@ public class CircleArea {
 		return area;
 	}
 
+  public static Double calcHalfArea(double r) {
+		double area = (Math.PI * (r * r))/2;
+		return area;
+	}
+
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Write a radio to calculate area of circle: ");
 		do {
 			double radio = sc.nextDouble();
 			System.out.println("Area of circle: " + calcArea(radio));
+      System.out.println("Half Area of circle: " + calcHalfArea(radio));
 		} while (sc.hasNext());
 	}
 }
